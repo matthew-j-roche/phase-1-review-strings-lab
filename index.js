37d9816efaeddcea042199edccce20b91f258c9e
matt roche <matt.roche@gmail.com>
2023-03-17 20:17:37 -0500

    Completed phase 1 review strings lab

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// Write your code in this file!
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook,  + ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const firstInitial = currentUser[0];
+
+const restOfName = currentUser.slice(0);
+
+const shortGreeting = `Welcome, + ${firstInitial}!`;
+
